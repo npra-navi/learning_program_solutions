5a302e0e44915776c5167cc33f634d0cdf242316
Pranavi Naru <pranavireddyn1104@gmail.com>
2025-06-21 22:08:36 +0530

    week_1_SingletonPattern

---
+++ SingletonTest.java
@@ -0,0 +1,18 @@
+public class SingletonTest {
+    public static void main(String[] args) {
+       
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First log message.");
+
+        
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second log message.");
+
+       
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same.");
+        } else {
+            System.out.println("Logger instances are different. Singleton pattern is not implemented correctly.");
+        }
+    }
+}
